cc7c029199b415941e4f5da9f9a4055db4cc2715
ty <towyuan@outlook.com>
2021-08-08 21:27:00 +0800

    chore: add comment for 'data'

--- packages/server/src/connection.ts
+++ packages/server/src/connection.ts
@@ -1,5 +1,8 @@
 import {ServerSocket} from '@remly/core';
 
 export class Connection extends ServerSocket {
+  /**
+   * Additional information that can be attached to the Connection instance and which will be used in DTO/Persistent
+   */
   public data: Record<string, any> = {};
 }
