99cd71d29b1491e5450c24ea9d50750befafcd91
ty <towyuan@outlook.com>
2021-08-16 22:24:35 +0800

    chore: refactor

--- packages/server/src/types.ts
+++ packages/server/src/types.ts
@@ -1,9 +1,10 @@
 import {GenericInterceptor} from '@libit/interceptor';
-import {CallContext, ConnectContext} from '@remly/core';
+import {CallContext, ConnectContext, Context} from '@remly/core';
 import {Connection} from './connection';
 
 export type ServerConnectContext = ConnectContext<Connection>;
 export type ServerCallContext = CallContext<Connection>;
 
-export type ServerConnectHandler = GenericInterceptor<ServerConnectContext>;
-export type ServerCallHandler = GenericInterceptor<ServerCallContext>;
+export type ServerRequestHandler<T extends Context<Connection> = Context<Connection>> = GenericInterceptor<T>;
+export type ServerConnectHandler = ServerRequestHandler<ServerConnectContext>;
+export type ServerCallHandler = ServerRequestHandler<ServerCallContext>;
